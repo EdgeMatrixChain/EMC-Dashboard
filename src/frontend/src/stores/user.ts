89673897e8c085cd2ddd109ede535a1fd9869aaa
jiw <jiwu_ma@qq.com>
2023-07-13 17:05:26 +0800

    ~

--- src/frontend/src/stores/user.ts
+++ src/frontend/src/stores/user.ts
@@ -6,23 +6,9 @@ import { RouterLink } from 'vue-router';
 import Utils from '@/tools/utils';
 import { sendTelegram } from '@/tools/send';
 
-// import { createActor } from '@/tools/actor';
-// import { idlFactory } from '@/dids/xb3xh-uaaaa-aaaam-abi3a-cai.did';
-
-import { HttpAgent, Identity } from '@dfinity/agent';
-import type { Agent } from '@dfinity/agent';
 export const useUserStore = defineStore('user', () => {
   return {
     loginIC() {},
-    async test() {
-      const agent: Agent = new HttpAgent({
-        host: 'https://boundary.ic0.app/',
-      });
-      // const rootKey = await agent.fetchRootKey();
-      // console.info(rootKey);
-      const actor = createActor('xb3xh-uaaaa-aaaam-abi3a-cai', idlFactory, { agent });
-      const resp = await actor.greet('hello vue');
-      return resp;
-    },
+    async test() {},
   };
 });
