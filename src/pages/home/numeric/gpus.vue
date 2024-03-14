4714544ddbe7c5d74ca3747411b450a3fb4dc9c6
jiw <jiwu_ma@qq.com>
2024-03-14 15:52:43 +0800

    update gpus query params

--- src/pages/home/numeric/gpus.vue
+++ src/pages/home/numeric/gpus.vue
@@ -16,9 +16,11 @@ const tips = ref('');
 const loading = ref(false);
 onMounted(async () => {
   loading.value = true;
+  const now = new Date().getTime();
   const resp = await http.get({
     url: '/stats/nodecpus',
-  }); 
+    data: { updatetimebegin: now - 360 * 60000, updatetimeend: now + 60 * 60000 },
+  });
   loading.value = false;
   const data = resp.data || {};
   value.value = formatNumber(data.gpu || 0);
