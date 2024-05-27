afcd5f64b3be1e3e5d954b8cfea769c30a822424
jiw <jiwu_ma@qq.com>
2024-05-27 17:17:15 +0800

    update fdv

--- src/pages/home/tokenomics-arb/token-fdv.vue
+++ src/pages/home/tokenomics-arb/token-fdv.vue
@@ -7,6 +7,7 @@ import { ref, onMounted } from 'vue';
 import NumericBasic from '@/pages/home/numeric/basic-simple.vue';
 import { formatNumber, formatMillion } from '@/tools/format-number';
 import { getDexData } from '@/apis';
+import { http } from '@/tools/http';
 const title = ref('FDV');
 const value = ref('');
 const unit = ref('');
@@ -14,9 +15,10 @@ const tips = ref('');
 const loading = ref(false);
 onMounted(async () => {
   loading.value = true;
-  const { fdv, priceUsd } = await getDexData();
+  const [resp, { fdv, priceUsd }] = await Promise.all([http.get({ url: '/stats/total-supply-all' }), getDexData()]);
   loading.value = false;
-  const { value: _value, text, unit: _unit } = formatMillion(fdv);
+  const data = (resp.data || {}) as { [k: string]: { raw: string; num: number } };
+  const { value: _value, text, unit: _unit } = formatMillion(data.arb.num * priceUsd);
   value.value = `$${text}`;
 });
 </script>
